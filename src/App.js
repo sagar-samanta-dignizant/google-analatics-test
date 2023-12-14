00627499508ae9fb6f4c2a64439ce44e75e81f3b
Sagar Samanta <sagar.samanta@dignizant.com>
2023-12-14 11:49:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const hanldleClick = () => {
+    setCount((prev) => prev + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ dislapy: 'flex' }}>
+      <div>Testing App</div>
+      <div>Count= {count}</div>
+      <button onClick={() => hanldleClick()}>Submit</button>
     </div>
   );
 }
