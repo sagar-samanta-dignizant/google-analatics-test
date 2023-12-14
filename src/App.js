62b9d87f719f1660385df72e8c0eead61b8a4693
Sagar Samanta <sagar.samanta@dignizant.com>
2023-12-14 17:03:38 +0530

    second 2 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import ReactGA from 'react-ga'
 
-ReactGA.initialize("G-ZL9VZME60B")
-
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname
-})
 function App() {
   const [count, setCount] = useState(0)
   const hanldleClick = () => {
@@ -15,7 +8,7 @@ function App() {
   }
   return (
     <div className="App" style={{ dislapy: 'flex' }}>
-      <div>Testing App 2</div>
+      <div>Testing App 24444</div>
       <div>Count= {count}</div>
       <img src="/logo192.png"  alt='NOT'/>
       <button style={{height:'100px',width:'100px',cursor:'pointer'}} onClick={() => hanldleClick()}>Download</button>
